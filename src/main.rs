466873ca4c4f5b0461dc6bc1be30ccdff38b75af
Sourav_Bhattacharya10 <sourav.bhattacharya3@gmail.com>
2024-09-14 17:09:10 +0530

    GET all todos done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+mod entities;
+mod error_responder;
+mod setup;
+mod todos;
+mod cors;
+
+use rocket::*;
+
+use crate::setup::setup_db;
+use todos::todo_routes::get_all_todos;
+use crate::cors::Cors;
+
+#[launch] // The "main" function of the program
+async fn rocket() -> _ {
+    let db_conn = match setup_db().await {
+        Ok(db) => db,
+        Err(err) => panic!("{}", err),
+    };
+
+    rocket::build()
+        .manage(db_conn)
+        .attach(Cors)
+        .mount("/", routes![health])
+        .mount("/todos", routes![get_all_todos])
+}
+
+#[get("/health")]
+async fn health() -> &'static str {
+    "OK Hello World"
 }
